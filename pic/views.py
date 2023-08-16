b92c69d08b9a5373498e725609e83330c4836618
Mariana Gutierrez <marianaguja236@gmail.com>
2023-08-16 08:42:06 -0500

    cambios pequeños

--- pic/views.py
+++ pic/views.py
@@ -6,19 +6,7 @@ from .forms import ImageUploadForm
 from pic.models import Image
 
 # Create your views here.
-'''
-def home(request):
-    if request.method == 'POST':
-        form = DocumentoForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')#redirigue a donde deseas
-    else:
-        form = DocumentoForm()
-    return render(request, 'home.html', {
-        'form': form
-    })
-'''
+
 def upload_image(request):
     mis_imagenes = Image.objects.all()
     if request.method == 'POST':
@@ -30,5 +18,7 @@ def upload_image(request):
     else:
         image_form = ImageUploadForm()
         return render(request, 'home.html',{'image_form':image_form, 'usuarios':mis_imagenes} )
+    
+    
     
 
