26d66222d78ba03ef2e20901911a26e91651fa70
Mariana Gutierrez <marianaguja236@gmail.com>
2023-10-18 01:23:54 -0500

    agregando cambios en el front

--- PicStrainer/settings.py
+++ PicStrainer/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'pics')
+MEDIA_ROOT = os.path.join(BASE_DIR,'p   ics')
 MEDIA_URL = '/pics/'
 
 
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+    'PicStrainer/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
